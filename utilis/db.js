34e1475598b6d071b4f37ea85b06dd5d5156b889
Abel Kashoba <lebakashoba@hotmail.fr>
2023-03-11 18:16:41 +0100

    Fix a type of dynamodEndpoint variable

--- utilis/db.js
+++ utilis/db.js
@@ -2,7 +2,7 @@ import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
 const dynamoEndpoint = process.env.DYNAMODB_ENDPOINT,
-endpoint = (dynamodEndpoint)? `https://${dynamoEndpoint}`:'http://localhost:8000';
+endpoint = (dynamoEndpoint)? `https://${dynamoEndpoint}`:'http://localhost:8000';
 
 export const client = new DynamoDBClient({ region:'us-east-1', endpoint });
 export const clientD = DynamoDBDocumentClient.from(client);
