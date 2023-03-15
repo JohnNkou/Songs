151dafc263cbdc395f3a4acc8c3e6afbab24c29b
Abel Kashoba <lebakashoba@hotmail.fr>
2023-03-15 15:40:26 +0100

    Additional middleware were added in the store ( saveUiInfo, checkReachability, logAction, ManageFastAccess);
    The file will make two export(store and fAccess)

--- utilis/store.js
+++ utilis/store.js
@@ -1,3 +1,16 @@
-const { createStore } = require('redux');
-const { Reducer} = require('./reducers')
-const { addSong, removeSong, addCategorie, removeCategorie, setCurrentCat, setCurrentSong } = require('./aCreator')
+import { createStore,applyMiddleware } from 'redux';
+import { Reducer} from './newReducer.cjs';
+import { saveUiInfo, checkReachability, timeAction, logAction, myThunk, ManageFastAccess } from '../middleware/index.js';
+import { getStoreData, appState, curry, saveToLocalStorage } from './BrowserDb.cjs';
+import { System } from './constant.cjs';
+
+const storeData = getStoreData(appState),
+fastAccess = {},
+save = curry(saveToLocalStorage)(System.LOCALSTORAGE),
+saveUiInfoCurried = curry(saveUiInfo)(save),
+ManageFastAccessCurried = curry(ManageFastAccess)(fastAccess);
+
+
+export const store = createStore(Reducer,storeData,applyMiddleware(myThunk,ManageFastAccessCurried,checkReachability,saveUiInfoCurried,logAction));
+
+export const fAccess = fastAccess;
