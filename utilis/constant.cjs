eaf225798d3d5dc401e56eab1feeb2631e4a2d99
Abel Kashoba <lebakashoba@hotmail.fr>
2023-03-16 16:54:14 +0100

    The following constant were added: 	- The SUB constant. contain api action messages 	- The appState is the initial state of the Redux store 	- The insertStatus. contain variable that represent the status code of operaionts. 	- The signal contain signals strings. 	- The displayTime, contain number that represent the number ms.

--- utilis/constant.cjs
+++ utilis/constant.cjs
@@ -40,18 +40,109 @@ exports.C = {
 	APP_UNREACHABLE: "APP UNREACHABLE",
 	INITIALIZE_FAVORITE: "INITIALIZE FAVORITE"
 }
-
+exports.SUB = {
+	UPDATE: "UPDATE",
+	DELETE: "DELETE",
+	UNSUBSCRIBE: "UNSUBSCRIBE",
+	ADD: "ADD",
+	NOTHING: "NOTHING",
+	UPLOADVERSES: "UPLOADVERSES",
+	STREAMDELETED: "STREAMDELETED",
+	CHANGED_SONG: "CHANGED SONG",
+	REGISTRATION_STARTING: " REGISTRATION STARTING",
+	REGISTRATION_INSTALLING: "REGISTRATION INSTALLING",
+	REGISTRATION_DONE: "REGISTRATION DONE",
+	REGISTRATION_ACTIVE: "REGISTRATION ACTIVE"
+}
+exports.appState = {
+	Categories:[],
+	onlineSongs:{},
+	offlineSongs:{},
+	currentCat:{name:""},
+	currentSong:{name:"", verses:[]},
+	ui:{
+		show:{
+			catList:false,
+			favList:false,
+			streamList:false,
+			settingList:false,
+			resultList:false,
+			addCatDiv:false,
+			addSongDiv:false,
+			createStreamDiv:false,
+			devTool:false
+		},
+		navigation:{
+			verseIndex:0,
+			to:20
+		},
+		addSongDiv:{
+			verses:0
+		},
+		direction:"Right",
+		nightMode: false
+	},
+	images:{
+		download: "download.png",
+		streamCreate: {
+			start: "streamStart.png",
+			stop: "streamStop.png"
+		},
+		categorie:"cat.png",
+		favorite:{
+			start:"favorite.png",
+			love:"love.png",
+			unlove:"unlove.png"
+		},
+		streamList:{
+			banner:"stream.png",
+			open:"openStream.png",
+			showed:""
+		},
+		arrows:{
+			next: "next.png",
+			prev: "prev.png"
+		}
+	},
+	subscribedToStream:false,
+	keys:{
+		alt:false
+	},
+	updateForced:{
+		songList:false,
+		content:false,
+		catNames:false,
+		settings: false,
+	},
+	language:"Fr",
+	favorites:{},
+	searchResult:[],
+	message:"",
+	selector:{
+		withVerse:false
+	},
+	songIncrement:100,
+	isStreaming:false,
+	appReachable:true,
+}
 exports.System = {
 	LOCALSTORAGE: 'LOCAL'
-}
-exports.databasesOptions = {
-	host: 		'localhost',
-	user: 		'Abel',
-	password: 	'Abel',
-	database: 	'Songs'
 }
 exports.dev = {
 	START_SQL_FILE: '/Users/flashbell/Node/Songs/utilis/startSql.js',
 	LOG_FILE: 		'/var/log/Songs/modain.log',
 	ERROR_LOG_FILE: '/var/log/Songs/error.log'
 }
+exports.insertStatus = {
+	FAILED:0,
+	SUCCESS:1,
+	DUPLICATE:2,
+	COMPLETE:3,
+	FAIL_ALL:4
+}
+exports.signal = {
+	system: "mSystem",
+	success:"mSuccess",
+	error:  "mError"
+}
+exports.displayTime = { fast:50, normal:1500, medium:3000, long:20000}
